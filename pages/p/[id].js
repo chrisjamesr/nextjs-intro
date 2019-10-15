cee2bd367f46b227f542548f03417c0e24525b49
chrisjamesr <chrisjames.r@gmail.com>
2019-10-14 22:17:16 -0400

    modify path for MyLayout component

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import Layout from '../components/MyLayout';
+import Layout from '../../components/MyLayout';
 
 const Post = () => {
   const router = useRouter();
