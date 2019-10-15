157b23eebea7e5fe8afe141cd56156c64e7cc415
chrisjamesr <chrisjames.r@gmail.com>
2019-10-14 20:41:24 -0400

    Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Header from "../components/Header";
+import Layout from "../components/MyLayout";
 
 export default function Index(){ 
   return (
-    <div>
-      <Header />
+    <Layout>
       <p>Hello Next.js </p>
-    </div>
+    </Layout>
   );
 }
