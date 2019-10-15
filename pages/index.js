bf3650828f455c6ab52dba1c0e7f0e2746ad4b91
chrisjamesr <chrisjames.r@gmail.com>
2019-10-15 13:27:32 -0400

    Modify index page content. Add CSS styling

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,70 @@ import Layout from "../components/MyLayout";
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {
-        props.shows.map(show => (
-          <li key={show.id}>
-            <Link href="/p/[id]" as={`/p/${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
-        ))
-      }
-    </ul>
-  </Layout>
-);
 
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data.map(entry => entry.show)
-  };
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
 };
 
-export default Index;
+export default function Blog(){
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {
+          getPosts().map(post => (
+            <li key={post.id}>
+              <Link href="/p/[id]" as={`/p/${post.id}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))
+        }
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
+  )
+};
+
+// Index.getInitialProps = async function() {
+//   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//   const data = await res.json();
+
+//   console.log(`Show data fetched. Count: ${data.length}`);
+
+//   return {
+//     shows: data.map(entry => entry.show)
+//   };
+// };
+
+// export default Index;
 
 
 // const PostLink = props => (
